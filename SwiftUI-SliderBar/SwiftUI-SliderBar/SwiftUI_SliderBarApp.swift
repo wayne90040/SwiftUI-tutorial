5067679c23b944678f113d2587de6170459b042e
HSU WEI LUN <wayne90040@gmail.com>
2022-02-16 11:16:21 +0800

    🎉 Start `SwiftUI-SliderBar`

---
+++ SwiftUI-SliderBar/SwiftUI-SliderBar/SwiftUI_SliderBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SliderBarApp.swift
+//  SwiftUI-SliderBar
+//
+//  Created by Wei Lun Hsu on 2022/2/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SliderBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
