8af323563015fcfbfc180ad3ca80a7562e11f935
HSU WEI LUN <wayne90040@gmail.com>
2022-05-30 23:25:32 +0800

    chore(circle-clock): first commit

---
+++ circle-clock/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Wei Lun Hsu on 2022/5/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ circle-clock/Shared/circle_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  circle_clockApp.swift
+//  Shared
+//
+//  Created by Wei Lun Hsu on 2022/5/30.
+//
+
+import SwiftUI
+
+@main
+struct circle_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
