4b638149e56243213b241b525a28fc14e23612c3
HSU WEI LUN <wayne90040@gmail.com>
2022-06-02 00:14:53 +0800

    feat: add timer

--- circle-clock/Shared/ContentView.swift
+++ circle-clock/Shared/ContentView.swift
@@ -20,28 +20,60 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct Watch: View {
+    @State var date: Date = Date()
+    private var timer: Timer?
+    
     var body: some View {
-        ZStack {
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.hour, .minute, .second], from: date)
+        
+        var minAngle: Double = 0
+        var hourAngle: Double = 0
+        var secondAngle: Double = 0
+        
+        if let hour = dateComponents.hour,
+           let minute = dateComponents.minute,
+           let second = dateComponents.second
+        {
+            let radianInHour = 2 * Double.pi / 12
+            let radianInOneMinute = 2 * Double.pi / 60
+            let actualHour = Double(hour) + (Double(minute) / 60)
+            hourAngle = actualHour + radianInHour
+            minAngle = Double(minute) * radianInOneMinute
+            secondAngle = Double(second) * radianInOneMinute
+        }
+        
+        return ZStack {
             Arc(start: .radians(0), end: .radians(Double.pi * 2))
                 .stroke(lineWidth: 3)
             Ticks()
             Numbers()
             Circle()
                 .frame(width: 15, height: 15, alignment: .center)
-            Hand(offset: 10)
+            Hand(offset: 30)
                 .frame(width: 4, alignment: .center)
+                .rotationEffect(.radians(hourAngle))
             Hand(offset: 10)
-                .frame(width: 4, alignment: .center)
+                .frame(width: 3, alignment: .center)
+                .rotationEffect(.radians(minAngle))
             Hand(offset: 5)
                 .foregroundColor(.red)
                 .frame(width: 2, alignment: .center)
+                .rotationEffect(.radians(secondAngle))
             Circle()
                 .foregroundColor(.red)
                 .frame(width: 7, height: 7, alignment: .center)
         }
-        .frame(width: 200, height: 200, alignment: .center)
-        
+        .frame(width: 200, height: 200, alignment: .leading)
+        .onAppear(perform: start)
     }
+    
+    private func start() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.date = Date()
+        }
+    }
+
 }
 
 struct Ticks: View {
